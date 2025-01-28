c4fb9c0e94ad7289e4efa8f2345d4017d1559e0f
Harshit Shrivastava <hrshitsrivastava0@gmail.com>
2025-01-28 20:41:49 +0530

    Deployment try 3

--- Backend/Frontend/src/components/Furniture.jsx
+++ Backend/Frontend/src/components/Furniture.jsx
@@ -1,76 +1,73 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick"; 
-import axios from 'axios';
-import Cards from './Cards';
+import Slider from "react-slick";
+import axios from "axios";
+import Cards from "./Cards";
 function Furniture() {
-  const [furniture,setFurniture] = useState([])
-    useEffect(()=>{
-        const getFurniture =async()=>{
-            try {
-                const res = await axios.get("http://localhost:4001/furniture");
-                console.log(res.data);
-                const data= res.data.filter((data)=>data.where === "Home");
-                setFurniture(data);
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        getFurniture();
-    },[])  
-    var settings = {
-        dots: true,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        initialSlide: 0,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2,
-              initialSlide: 2
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-        ]
-      };
-  return (<>
-    <div className='max-w-screen-2xl container mx-auto md:px-20 px-4'>
-        <h1 className='text-3xl font-semibold pb-2'>Our Products</h1>
-    </div>
-    <div className='max-w-screen-2xl container mx-auto md:px-20 px-4'>
-    <Slider {...settings}>
-        {
-            furniture.map((item)=>(
-                <Cards item={item} key={item.id}/>
-            ))
-        }
-    </Slider>
-    </div>
-
-  </>
-  )
+  const [furniture, setFurniture] = useState([]);
+  useEffect(() => {
+    const getFurniture = async () => {
+      try {
+        const res = await axios.get("/furniture");
+        console.log(res.data);
+        const data = res.data.filter((data) => data.where === "Home");
+        setFurniture(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getFurniture();
+  }, []);
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <>
+      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <h1 className="text-3xl font-semibold pb-2">Our Products</h1>
+      </div>
+      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <Slider {...settings}>
+          {furniture.map((item) => (
+            <Cards item={item} key={item.id} />
+          ))}
+        </Slider>
+      </div>
+    </>
+  );
 }
 
-export default Furniture
-
+export default Furniture;
--- Backend/Frontend/src/components/Login.jsx
+++ Backend/Frontend/src/components/Login.jsx
@@ -10,37 +10,42 @@ function Login() {
     formState: { errors },
   } = useForm();
 
-  const onSubmit =async (data) => {
+  const onSubmit = async (data) => {
     const userInfo = {
-    email:data.email,
-    password:data.password
-  }
-  await axios.post("http://localhost:4001/user/login",userInfo)
-  .then((res)=>{
-    console.log(res.data)
-    if(res.data){
-      toast.success('Login Successfull!');
-      setTimeout(() => {
-      document.getElementById("my_modal_3").close();
-      window.location.reload();
-      localStorage.setItem("Users",JSON.stringify(res.data.user))
-      }, 1000);
-    }
-  }).catch((err)=>{
-    if(err){
-    console.log(err)
-    toast.error('Error: '+err.response.data.message);
-    setTimeout(() => {}, 1000);
-    }
-  });
-}
+      email: data.email,
+      password: data.password,
+    };
+    await axios
+      .post("/user/login", userInfo)
+      .then((res) => {
+        console.log(res.data);
+        if (res.data) {
+          toast.success("Login Successfull!");
+          setTimeout(() => {
+            document.getElementById("my_modal_3").close();
+            window.location.reload();
+            localStorage.setItem("Users", JSON.stringify(res.data.user));
+          }, 1000);
+        }
+      })
+      .catch((err) => {
+        if (err) {
+          console.log(err);
+          toast.error("Error: " + err.response.data.message);
+          setTimeout(() => {}, 1000);
+        }
+      });
+  };
   return (
     <div>
       <dialog id="my_modal_3" className="modal">
         <div className="modal-box dark:bg-slate-900 dark:text-white">
           <form onSubmit={handleSubmit(onSubmit)} method="dialog">
             {/* if there is a button in form, it will close the modal */}
-            <a href="/" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+            <a
+              href="/"
+              className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+            >
               ✕
             </a>
             <h3 className="font-bold text-lg">Login</h3>
@@ -54,8 +59,7 @@ function Login() {
                 {...register("email", { required: true })}
               />
               <br />
-              {errors.email && (<span>*This field is required</span>
-              )}
+              {errors.email && <span>*This field is required</span>}
             </div>
             <div className="mt-4 space-y-2 dark:bg-slate-900 dark:text-white">
               <span>Password</span>
@@ -75,8 +79,11 @@ function Login() {
               </button>
               <p>
                 New user?{" "}
-                <Link to="/signup" className="text-blue-500 underline cursor-pointer hover:bg-blue-700 duration-300">
-                Sign up
+                <Link
+                  to="/signup"
+                  className="text-blue-500 underline cursor-pointer hover:bg-blue-700 duration-300"
+                >
+                  Sign up
                 </Link>
               </p>
             </div>
